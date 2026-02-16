fab1661fb7d1763c9893fb3bbee4948ef04fea34
Sarah Brofeldt <sarah@qtr.dk>
2026-02-16 06:37:17 +0100

    linux v6.18.6 -> v6.18.10, isp4: v7 -> v8

--- flake.nix
+++ flake.nix
@@ -6,20 +6,26 @@
         boot.kernelPackages =
           let
             linux = pkgs.buildLinux rec {
-              version = "6.18.6";
+              version = "6.18.10";
               modDirVersion = version;
-              src = pkgs.fetchFromGitHub {
-                owner = "srhb";
-                repo = "linux";
-                rev = "3be91244852a6ed74b5c1363098fdd5084290cc4";
-                hash = "sha256-+gTM7j64JQig6ScEa4u6hghdvz9dxXLNOeSTX0OxqV0=";
+              src = fetchTarball {
+                url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.18.10.tar.xz";
+                sha256 = "sha256:1jrh1ylkaivgyqgdx72r3f9wnfffmvg3bzw37k84bq53z09i7zpd";
               };
             };
             kernelPackages = pkgs.linuxPackagesFor linux;
           in
-          kernelPackages.extend (
-            self: super: { }
-          );
+          kernelPackages;
+
+        boot.kernelPatches = [
+          {
+            name = "v8_20260212_bin_du_add_amd_isp4_driver";
+            patch = pkgs.fetchurl {
+              url = "https://lore.kernel.org/all/20260212083426.216430-1-Bin.Du@amd.com/t.mbox.gz";
+              hash = "sha256-kvwjOYB478OjLT81+I3I4gEcIRXhdTuv1ofP3/DgpEs=";
+            };
+          }
+        ];
       };
   };
 }
