af2407f60ab7b5941b85e19ca8c7eac8d9e09f45
Sarah Brofeldt <sarah@qtr.dk>
2026-03-04 13:33:55 +0100

    Restore suspend functionaliry with isp4v7, linux-firmware bump and 6.18.10. Unsure which are relevant really

--- flake.nix
+++ flake.nix
@@ -6,20 +6,38 @@
         boot.kernelPackages =
           let
             linux = pkgs.buildLinux rec {
-              version = "6.18.3";
+              version = "6.18.10";
               modDirVersion = version;
-              src = pkgs.fetchFromGitHub {
-                owner = "srhb";
-                repo = "linux";
-                rev = "ef0493b6a080b30e82daa468e569f79ab37443b2";
-                hash = "sha256-h1VYUMkINfLSNn9xPBeNuXj26FcevAhq1EmGvQ2vWhI=";
+              src = fetchTarball {
+                url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${version}.tar.xz";
+                sha256 = "sha256:1jrh1ylkaivgyqgdx72r3f9wnfffmvg3bzw37k84bq53z09i7zpd";
               };
             };
             kernelPackages = pkgs.linuxPackagesFor linux;
           in
-          kernelPackages.extend (
-            self: super: { }
-          );
+          kernelPackages;
+
+        boot.kernelPatches = [
+          {
+            name = "v7_20251216_bin_du_add_amd_isp4_driver";
+            patch = pkgs.fetchurl {
+              url = "https://lore.kernel.org/all/20251216091326.111977-1-Bin.Du@amd.com/t.mbox.gz";
+              hash = "sha256-NZ04mjoij4Teh9VqAsw0RGpHtwmzYnZhSnPzVLasY5Y=";
+            };
+          }
+        ];
+
+        hardware.firmware = [
+          (pkgs.linux-firmware.overrideAttrs (_: {
+            version = "20260226";
+            src = pkgs.fetchFromGitLab {
+              owner = "kernel-firmware";
+              repo = "linux-firmware";
+              rev = "d8e138dd8970ffc9f5f879e2d62938abe6cd3f22";
+              hash = "sha256-/OkEh1xB8dud4Jun3eX3QjGeByJkfHxXNSVIctgoMyQ=";
+            };
+          }))
+        ];
       };
   };
 }
