919cffb465d134c1def3344572c37ded992c1641
Sarah Brofeldt <sarah@qtr.dk>
2026-05-06 07:42:36 +0200

    fix isp4 patch mbox hash

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
             name = "v7_20251216_bin_du_add_amd_isp4_driver";
             patch = pkgs.fetchurl {
               url = "https://lore.kernel.org/all/20251216091326.111977-1-Bin.Du@amd.com/t.mbox.gz";
-              hash = "sha256-NZ04mjoij4Teh9VqAsw0RGpHtwmzYnZhSnPzVLasY5Y=";
+              hash = "sha256-ncKvOyV8YzGJR0XdyspcH2tlanYeYnV68EstYcMax+o=";
             };
           }
         ];
