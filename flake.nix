7125007ad120a83cd696bfd83d37c1fed7a57972
Sarah Brofeldt <sarah@qtr.dk>
2026-05-20 10:11:52 +0200

    fix bluetooth on zbook g1a

--- flake.nix
+++ flake.nix
@@ -25,6 +25,16 @@
               hash = "sha256-PU0h7Wd3LC0vuXkWGd2YHAKTkGRvwZ79ZMkK3liea7A=";
             };
           }
+          {
+            # This is an unrelated problem to isp4 that fixes bluetooth on a
+            # zbook g1a, included because we're rebuilding anyway. It's queued
+            # for inclusion in 7.0.10
+            name = "btmtk-bluetooth-regression-fix";
+            patch = pkgs.fetchpatch2 {
+              url = "https://git.kernel.org/pub/scm/linux/kernel/git/stable/stable-queue.git/plain/queue-7.0/bluetooth-btmtk-accept-too-short-wmt-func_ctrl-events.patch";
+              hash = "sha256-q/sB4SLlqwtrnjmLKzkFa5B7sD8GDmhsZNd+hfcC1sY=";
+            };
+          }
         ];
       };
   };
