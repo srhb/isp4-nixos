f1e069b7c93e3c83fb4cfba0021e830db48ce3fd
Sarah Brofeldt <sarah@qtr.dk>
2026-06-08 08:51:46 +0200

    linux 7.0.11

--- flake.nix
+++ flake.nix
@@ -6,11 +6,11 @@
         boot.kernelPackages =
           let
             linux = pkgs.buildLinux rec {
-              version = "7.0.10";
+              version = "7.0.11";
               modDirVersion = version;
               src = fetchTarball {
                 url = "https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-${version}.tar.xz";
-                sha256 = "sha256:1ck1fyvr22221nj9rmyh52c8l74ykd86wjvxagfbp2cf3inzsl8c";
+                sha256 = "sha256:13pmv8p0lxbb2pqwhif2ilrkq04fmyh1k28w20y94vzcjv2rdfzj";
               };
             };
             kernelPackages = pkgs.linuxPackagesFor linux;
